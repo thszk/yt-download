d3802c8d0a435671448f245ed1902fb0253d20aa
thszk <thsuzuqui@gmail.com>
2021-01-22 09:26:48 -0400

    adiciona projeto inicial

---
+++ README.md
@@ -0,0 +1,26 @@
+# yt-download
+
+Ferramenta para realizar download de vídeos do YouTube e converterter para .mp3
+
+## Dependências
+
+| Nome    | Versão |
+|:-------:|:------:|
+| python  | 3.6.9  |
+| pip     | 9.0.1  |
+| pytube  | 10.4.1 |
+| moviepy | 1.0.3  |
+
+Se estiver em ambiente Linux é necessário ter instalado o ffmpeg.
+
+## Referências
+
+https://pypi.org/project/pytube/
+
+https://medium.com/@mklaben15/using-python-to-download-a-youtube-playlist-and-convert-to-mp3-for-a-custom-spotify-library-87ab950958a7
+
+https://simply-python.com/2019/01/02/downloading-youtube-videos-and-converting-to-mp3/
+
+## Linux Issues
+
+https://github.com/Zulko/moviepy/issues/1158#issuecomment-619224037
---
+++ yt-download.py
@@ -0,0 +1,58 @@
+#! /usr/bin/env python3
+
+import os, re, platform
+from pytube import YouTube
+from moviepy.editor import AudioFileClip
+
+# configurations
+if platform.system() == 'Linux':
+    os.environ['IMAGEIO_FFMPEG_EXE'] = '/usr/bin/ffmpeg'
+
+current_dir = os.path.dirname(__file__)
+output_dir = os.path.join(current_dir, 'output')
+input_file_path = os.path.join(current_dir, 'input.txt')
+log_file_path = os.path.join(current_dir, 'log.txt')
+
+print('>>>> matrix.py')
+print('>>>> output directory: ', current_dir)
+print('>>>> input file: ', input_file_path)
+
+# open files
+input_file = open(input_file_path, 'r', encoding='utf-8')
+log_file = open(log_file_path, 'w', encoding='utf-8')
+
+# download videos
+for url in input_file:
+    try:
+        print('... Baixando', url)
+        youtube = YouTube(url)
+
+        if youtube.author.endswith(' - Topic'):
+            name = youtube.author[:8] + ' - ' + youtube.title
+        else:
+            name = youtube.author + ' - ' + youtube.title
+
+        youtube.streams.get_audio_only().download(output_dir, name)
+    except Exception as e:
+        print('xxxx Erro ao baixar', url, e)
+        log_file.write('xxxx Erro ao baixar' + url)
+
+# convert to .mp3 and remove .mp4
+for mp4_file in os.listdir(output_dir):
+    try: 
+        if re.search('mp4', mp4_file):
+            mp4_file_path = os.path.join(output_dir, mp4_file)
+            mp3_file_path = os.path.join(output_dir, os.path.splitext(mp4_file)[0] + '.mp3')
+
+            mp3_file = AudioFileClip(mp4_file_path)
+            mp3_file.write_audiofile(mp3_file_path)
+
+            os.remove(mp4_file_path)
+    except Exception as e:
+        filepath = os.path.join(output_dir, mp4_file)
+        print('xxxx Erro ao converter', filepath, e)
+        log_file.write('xxxx Erro ao converter' + filepath + '\n')
+
+# close files
+input_file.close()
+log_file.close()
